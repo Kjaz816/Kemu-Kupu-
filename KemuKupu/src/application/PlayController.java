f6f36464761332f3b3f7c790627e88cf8f202711
oeng099 <oeng099@aucklanduni.ac.nz>
2021-09-22 15:45:59 +1200

    Added fxml componmenets to PlayController and linked them to Play.fxml

--- KemuKupu/src/application/PlayController.java
+++ KemuKupu/src/application/PlayController.java
@@ -4,9 +4,28 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class PlayController {
 	
 	private List<String> wordList = new ArrayList<String>();
+	@FXML
+	private Label score;
+	@FXML
+	private Label encouragingMessage;
+	@FXML
+	private Label word;
+	@FXML
+	private TextField userSpelling;
+	@FXML
+	private Button repeatWord;
+	@FXML
+	private Button check;
+	@FXML
+	private Button dontKnow;
 	
 	public void randWord(String topic) {
 		try {
